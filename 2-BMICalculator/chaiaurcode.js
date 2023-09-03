78c44b7b1b4276d814bebfe679eada2e6c3bae61
Vishal101022 <raivishal939@gmail.com>
2023-09-03 09:00:24 +0530

    added js for BMI Calculator

---
+++ 2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const result = document.querySelector('#results');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const h = Math.pow(parseInt(height.value) / 100, 2).toFixed(2);
+  const w = parseInt(weight.value);
+  // BMI formula weight / (height) m2
+  const finalResult = w / h;
+  // printing result
+  result.innerHTML = finalResult;
+});
